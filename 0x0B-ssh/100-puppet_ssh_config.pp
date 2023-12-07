b26b7c4c1d22ae6c4884d94ac18618c760c52828
Alex Nduta <wasremoved@gmail.com>
2023-12-07 21:25:24 +0300

    puppet set up for ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# configure to use a specific private key and signin without password
+file ('/home/was/.ssh/config'):
+    ensure => 'file',
+    owner => 'ubuntu',
+    group => 'ubuntu',
+    mode => '0600',
+    content =>"
+    Host 54.174.27.176,
+    IdentityFile ~/.ssh/school/
+    PasswordAuthetication no
+    ",
+}
