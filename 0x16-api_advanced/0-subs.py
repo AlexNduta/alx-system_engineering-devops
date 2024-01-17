67dbaacdf5615d3777d1fd29416b5b9c8febae67
Alex Nduta <wasremoved@gmail.com>
2024-01-17 07:45:57 +0300

    query the Reddit API to get subscribers: fixed the semi standard issues

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,13 +3,13 @@
     -if an invalid subredit is given, return 0
 """
 import requests
+
+
 def number_of_subscribers(subreddit):
     """ return number of subscribers in a given subreddit"""
-    url="https://www.reddit.com/r/{}/about.json".format(subreddit)
-    
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     rq = requests.get(url).json()
     subscriber = rq.get('data', {}).get('subscriber')
     if not subscriber:
         return 0
     return subscriber
-
